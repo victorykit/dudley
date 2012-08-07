963543d0764083aaa7f8058d3934ec78df0a2868
Aaron Swartz <me@aaronsw.com>
2012-08-07 08:33:36 -0400

    insert jobs into database

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import os, urllib, json
-from flask import Flask, request, make_response
-
+from flask import Flask, request, make_response, g
+import web
 
 app = Flask(__name__)
 app.config['DEBUG'] = bool(os.environ.get('DEBUG'))
+db = web.database()
 
 @app.route("/")
 def index():
@@ -11,10 +12,11 @@ def index():
 
 @app.route("/hook", methods=["POST"])
 def hook():
-    d = json.loads(request.data)
-    return "taking %s from %s to %s" % (
-      d['after'], d['before'], d['ref']
-    )
+    #d = json.loads(request.data)
+    d = request.json
+    if d['ref'] != 'refs/heads/master': return "skipping\n"
+    db.insert('jobs', commit_hash=d['after'])
+    return "queued\n"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
---
+++ schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE jobs (
+  id serial primary key,
+  commit_hash text,
+  builder text 
+);
