d42c97ec78bbee44878c54f066a5afce8b972a21
Aaron Swartz <me@aaronsw.com>
2012-08-07 09:14:10 -0400

    refactor courier

--- builder.py
+++ builder.py
@@ -1,15 +1,19 @@
 import os, web
-from courier import execute
-env = web.storage(os.environ)
+import courier
 
 REPO_STORAGE = '/tmp/dudley_repo'
 TMP_BUILDSERVER_NAME = 'sleepy-ocean-9027'
 
+env = web.storage(os.environ)
+
 def build(commit_hash):
+    sh = courier.Courier()
+    
     if not os.path.exists(REPO_STORAGE):
-        execute('git', 'clone', env.GIT_URL, REPO_STORAGE)
-    execute('git', 'pull', cwd=REPO_STORAGE)
-    execute('git', 'push', '-f', TMP_BUILDSERVER_NAME, commit_hash + ':master', cwd=REPO_STORAGE)
+        sh.run('git', 'clone', env.GIT_URL, REPO_STORAGE)
+    sh.cd(REPO_STORAGE)
+    sh.run('git', 'pull')
+    sh.run('git', 'push', '-f', TMP_BUILDSERVER_NAME, commit_hash + ':master')
     
 
 if __name__ == "__main__":
--- courier.py
+++ courier.py
@@ -3,11 +3,26 @@ courier: an intelligent subprocessor
 """
 import subprocess
 
-def execute(*args, **kwargs):
-    if kwargs.get('cwd'):
-        print '$ cd ' + kwargs['cwd']
-    print '$ ' + ' '.join(args)
-    kwargs['stdout'] = subprocess.PIPE
-    p = subprocess.Popen(args, **kwargs)
-    print p.stdout.read()
-    p.wait()
+class Courier:
+    def __init__(self):
+        self.cwd = None
+    
+    def cd(self, cwd):
+        self.note('cd ' + cwd)
+        self.cwd = cwd
+        
+    def run(self, *args):
+        self.note(' '.join(args))
+        p = subprocess.Popen(args, cwd=self.cwd, stdout=subprocess.PIPE)
+        self.output(p.stdout.read())
+        p.wait()
+        return p.returncode
+    
+    def _store(self, x):
+        print x
+    
+    def note(self, line):
+        self._store('$ ' + line)
+    
+    def output(self, data):
+        self._store(data)
