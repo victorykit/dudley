8bb1e6188b9412201667a2f14015265171531ec6
Aaron Swartz <me@aaronsw.com>
2012-08-07 07:53:52 -0400

    first stab at processing hooks

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import os, urllib
+import os, urllib, json
 from flask import Flask, request, make_response
 
 
@@ -8,6 +8,13 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Hello, world!"
+
+@app.route("/hook")
+def hook():
+    d = json.loads(request.data)
+    return "taking %s from %s to %s" % (
+      d['after'], d['before'], d['ref']
+    )
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
