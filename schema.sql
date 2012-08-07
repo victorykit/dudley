3579d0f2c23be894b9f589232a9cc7988be02cf1
Aaron Swartz <me@aaronsw.com>
2012-08-07 09:01:00 -0400

    update schema, .gitignore

--- schema.sql
+++ schema.sql
@@ -1,5 +1,20 @@
+DROP TABLE IF EXISTS jobs, builds, buildservers;
+
 CREATE TABLE jobs (
   id serial primary key,
   commit_hash text,
   builder text 
+);
+
+CREATE TABLE builds (
+  id serial primary key,
+  job_id int references jobs,
+  log text
+);
+
+CREATE TABLE buildservers (
+  id serial primary key,
+  git_url text,
+  short_name text,
+  currently_building int references builds
 );
